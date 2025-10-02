5b73f3a6b15641621a9ae7272f8f022ac3ca1ac7
Kilsup-Lee (이길섭) <gislee@dgu.ac.kr>
2025-10-02 14:35:39 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+print('hello world')  
+name='Kildong Hong'  
+
+print('I\'m {0}!'.format(name))  
+programming_language='Python'  
+print('The most popular language is {0}'.format(programming_language))
+
